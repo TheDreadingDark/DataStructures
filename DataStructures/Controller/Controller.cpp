c548be187469fb470b018eef32170ab3c54afb13
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2018-02-02 11:59:17 -0700

    Created findMaxAndMin method.

--- DataStructures/Controller/Controller.cpp
+++ DataStructures/Controller/Controller.cpp
@@ -12,21 +12,48 @@ using namespace std;
 
 void Controller :: start()
 {
-    Timer codeTimer;
-    codeTimer.startTimer();
-    cout << "Look code on the screen" << endl;
-    codeTimer.stopTimer();
-    codeTimer.displayInformation();
-    codeTimer.resetTimer();
-    codeTimer.startTimer();
+//    Timer codeTimer;
+//    codeTimer.startTimer();
+//    cout << "Look code on the screen" << endl;
+//    codeTimer.stopTimer();
+//    codeTimer.displayInformation();
+//    codeTimer.resetTimer();
+//    codeTimer.startTimer();
     
+//    vector<CrimeData> myData = FileController :: readCrimeDataToVector("/Users/awoo9855/Documents/Swift Projects/DataStructures/DataStructures/Data/crime.csv");
+    
+//    for (int index = 200; index < 216; index ++)
+//    {
+//        cout << index << " is " << myData[index] << endl;
+//    }
+    
+    findMaxAndMin();
+}
+    
+void Controller :: findMaxAndMin()
+{
+    Timer searchTimer;
+    searchTimer.startTimer();
     vector<CrimeData> myData = FileController :: readCrimeDataToVector("/Users/awoo9855/Documents/Swift Projects/DataStructures/DataStructures/Data/crime.csv");
-    
-    for (int index = 200; index < 216; index ++)
+        
+    int minIndex = 0;
+    int maxIndex = 0;
+        
+    for (int index = 1; index < myData.size(); index++)
     {
-        cout << index << " is " << myData[index] << endl;
+        if (myData [minIndex] < myData[index])
+        {
+            minIndex = index;
+        }
+            
+        if (myData [maxIndex] < myData[index])
+        {
+            maxIndex = index;
+        }
     }
     
-    codeTimer.stopTimer();
-    codeTimer.displayInformation();
+    searchTimer.stopTimer();
+    cout << "The smallest Crime stat is at " << minIndex << " and it is: " << myData[minIndex] << endl;
+    cout << "The largest Crime stat is at " << maxIndex << " and it is: " << myData[maxIndex] << endl;
+    searchTimer.displayInformation();
 }
