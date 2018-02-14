456d73451c14781e2d8b3c37f8fd43ac1d70107c
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2018-02-14 13:47:30 -0700

    Added Linked List code.

---
+++ DataStructures/Model/Structures/LinkedList.hpp
@@ -0,0 +1,106 @@
+//
+//  LinearNode.hpp
+//  DataStructures
+//
+//  Created by Woolsey, Adam on 2/14/18.
+//  Copyright © 2018 Woolsey, Adam. All rights reserved.
+//
+
+#ifndef LinkedList_hpp
+#define LinkedList_hpp
+
+#include "List.hpp"
+
+using namespace std; //Used for keyword access. Use this to make sure your pointers are pointing to the right things
+
+template <class Type>
+class LinkedList : public List<Type>
+{
+protected:
+    LinearNode<Type> * front;
+    LinearNode<Type> * end;
+public:
+    //Constructors
+    LinkedList();
+    virtual ~LinkedList(); //Virtual so it can be overridden.
+    //Helper methods
+    int getSize() const;
+    LinearNode<Type> * getFront();
+    LinearNode<Type> * getEnd();
+    
+    //Structure Methods
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type getFromIndex(int index);
+//    Type setAtIndex(int index, Type item);
+//    bool contains(Type item);
+};
+
+LinkedList<Type> :: LinkedList()
+{
+    this->front = nullptr;
+    this->end = nullptr;
+    this->size = 0;
+}
+
+LinkedList<Type> :: ~LinkedList()
+{
+    LinearNode<Type> * destroyStructure= front;
+    while (front != nullptr)
+    {
+        front = destroyStructure->getNextNode();
+        delete destroyStructure;
+        destroyStructure = front;
+    }
+    
+    void LinkedList<Type> :: add(Type item)
+    {
+        LinearNode<Type> * newData = new LinearNode<Type>(item);
+        
+        if(this->size == 0)
+        {
+            this->front = newData;
+        }
+        else
+        {
+            this->end->setNextNode(newData);
+        }
+        
+        this->end = newData;
+        
+        this->size += 1;
+    }
+    
+    void LinkedList<Type> :: addAtIndex(int index, Type item)
+    {
+        assert(index >= 0 && index <= this->size);
+        if(index == this->size)
+        {
+            add(item);
+        }
+        else
+        {
+            LinearNode<Type> * toBeAdded = new LinearNode<Type>(item);
+            if (index == 0)
+            {
+                toBeAdded->setNextNode(front);
+                front = toBeAdded;
+            }
+            else
+            {
+                LinearNode<Type> * previous = nullptr;
+                LinearNode<Type> * current = front;
+                for (int position = 0; position < index; position++)
+                {
+                    previous = current;
+                    current = current->getNextNode();
+                }
+                previous->setNextNode(toBeAdded);
+                toBeAdded->setNextNode(current);
+            }
+            this->size++;
+        }
+    }
+}
+
+#endif /* LinearNode_hpp */
