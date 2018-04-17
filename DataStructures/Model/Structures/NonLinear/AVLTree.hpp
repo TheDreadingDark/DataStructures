7c19e9888b93ed232d49e747d27f8229797f9d3b
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2018-04-17 12:53:08 -0600

    Added insertNode method.

--- DataStructures/Model/Structures/NonLinear/AVLTree.hpp
+++ DataStructures/Model/Structures/NonLinear/AVLTree.hpp
@@ -128,18 +128,6 @@ BinaryTreeNode<Type> * AVLTree<Type> :: balanceSubTree (BinaryTreeNode<Type> * p
 }
 
 template <class Type>
-void AVLTree<Type> :: insert(Type item)
-{
-    insertNOde(this->getRoot(), item);
-}
-
-template <class Type>
-void AVLTree<Type> :: remove(Type item)
-{
-    removeNode(this->getRoot(), item);
-}
-
-template <class Type>
 AVLTree<Type> :: AVLTree() : BinarySearchTree<Type>()
 {
     this->root = nullptr;
@@ -193,6 +181,36 @@ BinaryTreeNode<Type> * AVLTree<Type> :: removeNode(BinaryTreeNode<Type> * parent
 }
 
 template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: insertNode(BinaryTreeNode<Type> * parent, Type inserted)
+{
+    if(parent == nullptr)
+    {
+        parent = new BinaryTreeNode<Type>(inserted);
+        return parent;
+    }
+    else if(inserted < parent->getNodeData())
+    {
+        parent->setLeftChild(insertNode(parent->getLeftNode(), inserted));
+        parent = balanceSubTree(parent);
+    }
+    else if(inserted > parent->getNodeData())
+    {
+        parent->setRightChild(insertNode(parent->getRightNode(), inserte));
+        parent = balanceSubTree(parent);
+    }
+    return parent;
+}
 
+template <class Type>
+void AVLTree<Type> :: insert(Type item)
+{
+    insertNode(this->getRoot(), item);
+}
+
+template <class Type>
+void AVLTree<Type> :: remove(Type item)
+{
+    removeNode(this->getRoot(), item);
+}
 
 #endif /* AVLTree_hpp */
